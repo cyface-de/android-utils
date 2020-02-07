12984641bf74bf5482b88bc91766f986db9378f1
Armin <armin.schnabel@cyface.de>
2020-02-07 09:59:32 +0100

    Update licence to 'Cyface Utils'

--- utils/src/main/java/de/cyface/utils/TestEnvironment.java
+++ utils/src/main/java/de/cyface/utils/TestEnvironment.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2019 Cyface GmbH
  *
- * This file is part of the Cyface SDK for Android.
+ * This file is part of the Cyface Utils for Android.
  *
- * The Cyface SDK for Android is free software: you can redistribute it and/or modify
+ * The Cyface Utils for Android is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The Cyface SDK for Android is distributed in the hope that it will be useful,
+ * The Cyface Utils for Android is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with the Cyface SDK for Android. If not, see <http://www.gnu.org/licenses/>.
+ * along with the Cyface Utils for Android. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.cyface.utils;
 
@@ -24,7 +24,7 @@ import android.os.Build;
  * Contains methods used by multiple Cyface libraries to detect and react to test environments.
  *
  * @author Armin Schnabel
- * @version 1.0.0
+ * @version 1.0.1
  * @since 1.1.0
  */
 public class TestEnvironment {

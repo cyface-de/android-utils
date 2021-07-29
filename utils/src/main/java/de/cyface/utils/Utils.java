f20d451b38181cff5c12a22e6889b5aff77781cd
Armin <armin.schnabel@cyface.de>
2021-07-29 14:57:33 +0200

    [DAT-746] Upgrade dependencies and remove Validate (#37)
    * [DAT-746] Use java 11
    
    * Upgrade dependencies
    
    * Enable Java 11 support and remove Validate class
    
    * Update versions
    
    * Use JDK 11 in Github action
    
    * Upgrade APG to 7.0.0 and Gradle to 7.1.1

--- utils/src/main/java/de/cyface/utils/Utils.java
+++ utils/src/main/java/de/cyface/utils/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Cyface GmbH
+ * Copyright 2019-2021 Cyface GmbH
  *
  * This file is part of the Cyface Utils for Android.
  *
@@ -18,19 +18,19 @@
  */
 package de.cyface.utils;
 
-import java.io.File;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
 
+import java.io.File;
+
 /**
  * Static utility methods.
  *
  * @author Armin Schnabel
- * @version 1.0.1
+ * @version 1.0.2
  * @since 1.0.0
  */
 @SuppressWarnings("unused") // Used by app and camera service
@@ -39,13 +39,13 @@ public class Utils {
     /**
      * A utility method used to fix the bug that newly created files are not visible via MTP (usb data transfer).
      *
-     * @param context The current Android <code>Context</code> used to access media scanner.
+     * @param context    The current Android <code>Context</code> used to access media scanner.
      * @param targetFile The changed file to inform the media scanner about.
      */
     @SuppressWarnings("unused") // Used by app and camera service
     public static void informMediaScanner(@NonNull final Context context, @NonNull final File targetFile) {
-        final Intent mediaScannerIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        final Uri fileContentUri = Uri.fromFile(targetFile);
+        final var mediaScannerIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        final var fileContentUri = Uri.fromFile(targetFile);
         mediaScannerIntent.setData(fileContentUri);
         context.sendBroadcast(mediaScannerIntent);
     }

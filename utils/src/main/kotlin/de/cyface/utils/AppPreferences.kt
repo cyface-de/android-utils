49c867401259331c2ae08d8a249ee66ad61868d9
Armin <armin.schnabel@cyface.de>
2023-08-24 14:12:57 +0200

    [RFR-769] Migrate AppPreferences to Proto Datastore

--- utils/src/main/kotlin/de/cyface/utils/AppPreferences.kt
+++ utils/src/main/kotlin/de/cyface/utils/AppPreferences.kt
@@ -19,193 +19,173 @@
 package de.cyface.utils
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.MultiProcessDataStoreFactory
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.io.File
 
 /**
- * A helper class for managing and interacting with the SharedPreferences in the application.
+ * Common preferences used by all our UIs.
+ *
+ * Attention:
+ * - The preferences can be accessed from multiple processes.
+ * - Only create one instance of `DataStore` per file in the same process.
+ * - We use ProtoBuf to ensure type safety. Rebuild after changing the .proto file.
+ * - Never mix SingleProcessDataStore with MultiProcessDataStore for the same file.
  *
  * @author Armin Schnabel
- * @version 1.0.0
+ * @version 2.0.0
  * @since 3.4.0
- * @param context The context to access the SharedPreferences from.
+ * @param context The context to access the preferences from.
  */
 @Suppress("unused") // Part of the API
-open class AppPreferences(context: Context) {
+class AppPreferences(private val context: Context) {
 
     /**
-     * The preferences used to store the user's preferred settings.
+     * The data store with multi-process support.
      */
-    @Suppress("MemberVisibilityCanBePrivate") // Required by CustomPreferences extensions
-    val preferences: SharedPreferences = context.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+    private val dataStore: DataStore<Settings> = MultiProcessDataStoreFactory.create(
+        serializer = SettingsSerializer,
+        produceFile = {
+            File("${context.cacheDir.path}/app.preferences_pb")
+        }
+    )
 
     /**
-     * Saves the centerMap boolean value to SharedPreferences.
+     * Saves the centerMap boolean value.
      *
-     * @param centerMap The boolean value indicating whether the map should be centered.
+     * @param value The boolean value indicating whether the map should be centered.
      */
     @Suppress("unused") // Part of the API
-    fun saveCenterMap(centerMap: Boolean) {
-        preferences.edit {
-            putBoolean(PREFERENCES_CENTER_MAP_KEY, centerMap)
-            apply()
+    suspend fun setCenterMap(value: Boolean) {
+        dataStore.updateData { currentSettings ->
+            currentSettings.toBuilder()
+                .setCenterMap(value)
+                .build()
         }
     }
 
     /**
-     * Retrieves the centerMap boolean value from SharedPreferences.
-     *
      * @return The boolean value indicating whether the map should be centered.
      */
-    @Suppress("unused") // Part of the API
-    fun getCenterMap(): Boolean {
-        return preferences.getBoolean(PREFERENCES_CENTER_MAP_KEY, true)
-    }
+    val centerMapFlow: Flow<Boolean> = dataStore.data
+        .map { settings ->
+            settings.centerMap
+        }
 
     /**
-     * Saves the upload boolean value to SharedPreferences.
+     * Saves the upload boolean value.
      *
-     * @param upload The boolean value indicating whether data should be uploaded or synchronized.
+     * @param value The boolean value indicating whether data should be uploaded or synchronized.
      */
     @Suppress("unused") // Part of the API
-    fun saveUpload(upload: Boolean) {
-        preferences.edit {
-            putBoolean(PREFERENCES_SYNCHRONIZATION_KEY, upload)
-            apply()
+    suspend fun setUploadEnabled(value: Boolean) {
+        dataStore.updateData { currentSettings ->
+            currentSettings.toBuilder()
+                .setUploadEnabled(value)
+                .build()
         }
     }
 
     /**
-     * Retrieves the upload boolean value from SharedPreferences.
-     *
      * @return The boolean value indicating whether data should be uploaded or synchronized.
      */
     @Suppress("unused") // Part of the API
-    fun getUpload(): Boolean {
-        return preferences.getBoolean(PREFERENCES_SYNCHRONIZATION_KEY, true)
-    }
+    val uploadEnabledFlow: Flow<Boolean> = dataStore.data
+        .map { settings ->
+            settings.uploadEnabled
+        }
 
     /**
-     * Saves the sensor frequency to SharedPreferences.
+     * Saves the sensor frequency.
      *
-     * @param sensorFrequency The sensor frequency to safe.
+     * @param value The sensor frequency to safe.
      */
     @Suppress("unused") // Part of the API
-    fun saveSensorFrequency(sensorFrequency: Int) {
-        preferences.edit {
-            putInt(PREFERENCES_SENSOR_FREQUENCY_KEY, sensorFrequency)
-            apply()
+    suspend fun setSensorFrequency(value: Int) {
+        dataStore.updateData { currentSettings ->
+            currentSettings.toBuilder()
+                .setSensorFrequency(value)
+                .build()
         }
     }
 
     /**
-     * Retrieves the sensor frequency from the SharedPreferences.
-     *
-     * @return The sensor frequency.
+     * @return The maximum frequency with which the IMU sensor should collect data, e.g. 100 Hz.
      */
     @Suppress("unused") // Part of the API
-    fun getSensorFrequency(): Int {
-        return preferences.getInt(PREFERENCES_SENSOR_FREQUENCY_KEY, DEFAULT_SENSOR_FREQUENCY)
-    }
+    val sensorFrequencyFlow: Flow<Int> = dataStore.data
+        .map { settings ->
+            settings.sensorFrequency
+        }
 
     /**
-     * Saves if errors show be reported to SharedPreferences.
+     * Saves if errors show be reported.
      *
-     * @param reportingAccepted `True` is errors should be reported.
+     * @param value `True` is errors should be reported.
      */
     @Suppress("unused") // Part of the API
-    fun saveReportingAccepted(reportingAccepted: Boolean) {
-        preferences.edit {
-            putBoolean(ACCEPTED_REPORTING_KEY, reportingAccepted)
-            apply()
+    suspend fun setReportErrors(value: Boolean) {
+        dataStore.updateData { currentSettings ->
+            currentSettings.toBuilder()
+                .setReportErrors(value)
+                .build()
         }
     }
 
     /**
-     * Retrieves if errors should be reported from the SharedPreferences.
-     *
      * @return `True` if errors show be reported.
      */
     @Suppress("unused") // Part of the API
-    fun getReportingAccepted(): Boolean {
-        return preferences.getBoolean(ACCEPTED_REPORTING_KEY, false)
-    }
+    val reportErrorsFlow: Flow<Boolean> = dataStore.data
+        .map { settings ->
+            settings.reportErrors
+        }
 
     /**
-     * see [PREFERENCES_MODALITY_KEY]
+     * Saves the currently selected modality.
+     *
+     * @param value The modality.
      */
     @Suppress("unused") // Part of the API
-    fun saveModality(modality: String) {
-        preferences.edit {
-            putString(PREFERENCES_MODALITY_KEY, modality)
-            apply()
+    suspend fun setModality(value: String) {
+        dataStore.updateData { currentSettings ->
+            currentSettings.toBuilder()
+                .setModality(value)
+                .build()
         }
     }
 
     /**
-     * see [PREFERENCES_MODALITY_KEY]
+     * @return The currently selected modality, e.g. 'CAR'.
      */
     @Suppress("unused") // Part of the API
-    fun getModality(): String? {
-        return preferences.getString(PREFERENCES_MODALITY_KEY, null)
-    }
+    val modalityFlow: Flow<String> = dataStore.data
+        .map { settings ->
+            settings.modality
+        }
 
     /**
-     * see [ACCEPTED_TERMS_KEY]
+     * Saves the API version of the terms accepted by the user, e.g. 5.
+     *
+     * @param value The modality.
      */
     @Suppress("unused") // Part of the API
-    fun saveAcceptedTerms(acceptedTerms: Int) {
-        preferences.edit {
-            putInt(ACCEPTED_TERMS_KEY, acceptedTerms)
-            apply()
+    suspend fun setAcceptedTerms(value: Int) {
+        dataStore.updateData { currentSettings ->
+            currentSettings.toBuilder()
+                .setAcceptedTerms(value)
+                .build()
         }
     }
 
     /**
-     * see [ACCEPTED_TERMS_KEY]
+     * @return The API version of the terms accepted by the user, e.g. 5.
      */
     @Suppress("unused") // Part of the API
-    fun getAcceptedTerms(): Int {
-        return preferences.getInt(ACCEPTED_TERMS_KEY, 0)
-    }
-
-    companion object {
-        /**
-         * Default value in Hz when there is no user preference stored yet.
-         */
-        const val DEFAULT_SENSOR_FREQUENCY = 100
-
-        /*
-         * Preferences keys
-         *
-         * *Don't change the values, as this resets the user's preferences!*
-         */
-
-        /**
-         * The prefix used in the shared preferences, historically.
-         *
-         * *Don't change this, as this resets the user's preferences!*
-         */
-        private const val KEY_PACKAGE = "de.cyface.app"
-
-        /**
-         * Identifies the accepted terms version in the preferences.
-         */
-        const val ACCEPTED_TERMS_KEY = "$KEY_PACKAGE.accepted_terms"
-
-        /**
-         * Identifies if the user opted-in to error reporting in the preferences.
-         */
-        const val ACCEPTED_REPORTING_KEY = "$KEY_PACKAGE.accepted_reporting"
-
-        const val PREFERENCES_MODALITY_KEY = "$KEY_PACKAGE.modality"
-
-        const val PREFERENCES_CENTER_MAP_KEY = "$KEY_PACKAGE.zoom_to_location"
-        const val PREFERENCES_SYNCHRONIZATION_KEY = "$KEY_PACKAGE.synchronization_enabled"
-
-        /**
-         * The key for the `SharedPreferences`. This entry contains the preferred sensor frequency.
-         */
-        const val PREFERENCES_SENSOR_FREQUENCY_KEY = "$KEY_PACKAGE.sensor_frequency"
-    }
+    val acceptedTermsFlow: Flow<Int> = dataStore.data
+        .map { settings ->
+            settings.acceptedTerms
+        }
 }

bd756ed55eab00573f7b8dcae64a86bc1fd5d5de
Armin <armin.schnabel@cyface.de>
2023-09-18 09:36:21 +0200

    [RFR-769] Migrate to Proto DataStore (#67)
    * Upgrade robolectric to 4.10.2
    
    * Add datastore dependency
    
    * Upgrade kotlin to 1.9.10 (IDE upgrade required)
    
    * Add dependencies for Proto Datastore
    
    * [RFR-769] Migrate AppPreferences to Proto Datastore
    
    * [RFR-787] Add migration from sharedpreferences
    
    * [RFR-789] Set default values
    
    * Cleanup
    
    * Upgrade compileSdkVersion to 34 (required by datastore 1.1.0-alpha05

---
+++ utils/src/main/kotlin/de/cyface/utils/settings/PreferencesMigration.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 Cyface GmbH
+ *
+ * This file is part of the Cyface Utils for Android.
+ *
+ * The Cyface Utils for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Cyface Utils for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface Utils for Android. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cyface.utils.settings
+
+import android.content.Context
+import androidx.datastore.migrations.SharedPreferencesMigration
+import androidx.datastore.migrations.SharedPreferencesView
+import de.cyface.utils.Settings
+
+/**
+ * Factory for the migration which imports preferences from the previously used SharedPreferences.
+ *
+ * @author Armin Schnabel
+ * @version 1.0.0
+ * @since 3.6.0
+ */
+object PreferencesMigrationFactory {
+
+    /**
+     * The filename, keys and defaults of the preferences, historically.
+     *
+     * *Don't change this, this is migration code!*
+     */
+    private const val PREFERENCES_NAME = "AppPreferences"
+    private const val ACCEPTED_TERMS_KEY = "de.cyface.app.accepted_terms"
+    private const val ACCEPTED_REPORTING_KEY = "de.cyface.app.accepted_reporting"
+    private const val MODALITY_KEY = "de.cyface.app.modality"
+    private const val CENTER_MAP_KEY = "de.cyface.app.zoom_to_location"
+    private const val SYNCHRONIZATION_KEY = "de.cyface.app.synchronization_enabled"
+    private const val SENSOR_FREQUENCY_KEY = "de.cyface.app.sensor_frequency"
+    private const val DEFAULT_CENTER_MAP = true
+    private const val DEFAULT_SYNCHRONIZATION = true
+    private const val DEFAULT_REPORTING = false
+    private const val DEFAULT_ACCEPTED_TERMS = 0
+    private const val DEFAULT_SENSOR_FREQUENCY = 100
+    private const val DEFAULT_MODALITY = "UNKNOWN"
+
+    /**
+     * @param context The context to search and access the old SharedPreferences from.
+     * @return The migration code which imports preferences from the SharedPreferences if found.
+     */
+    fun create(context: Context): SharedPreferencesMigration<Settings> {
+        return SharedPreferencesMigration(
+            context,
+            PREFERENCES_NAME,
+            migrate = ::migratePreferences
+        )
+    }
+
+    private fun migratePreferences(
+        preferences: SharedPreferencesView,
+        settings: Settings
+    ): Settings {
+        return settings.toBuilder()
+            .setVersion(1) // Ensure the migrated values below are used instead of default values.
+            .setCenterMap(preferences.getBoolean(CENTER_MAP_KEY, DEFAULT_CENTER_MAP))
+            .setUploadEnabled(preferences.getBoolean(SYNCHRONIZATION_KEY, DEFAULT_SYNCHRONIZATION))
+            .setSensorFrequency(preferences.getInt(SENSOR_FREQUENCY_KEY, DEFAULT_SENSOR_FREQUENCY))
+            .setReportErrors(preferences.getBoolean(ACCEPTED_REPORTING_KEY, DEFAULT_REPORTING))
+            .setModality(preferences.getString(MODALITY_KEY, DEFAULT_MODALITY))
+            .setAcceptedTerms(preferences.getInt(ACCEPTED_TERMS_KEY, DEFAULT_ACCEPTED_TERMS))
+            .build()
+    }
+}
---
+++ utils/src/main/kotlin/de/cyface/utils/settings/StoreMigration.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2023 Cyface GmbH
+ *
+ * This file is part of the Cyface Utils for Android.
+ *
+ * The Cyface Utils for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Cyface Utils for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface Utils for Android. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cyface.utils.settings
+
+import android.util.Log
+import androidx.datastore.core.DataMigration
+import de.cyface.utils.Constants.TAG
+import de.cyface.utils.Settings
+
+/**
+ * Migration which ensures DataStore files from all versions are compatible.
+ *
+ * @author Armin Schnabel
+ * @version 1.0.0
+ * @since 3.6.0
+ */
+class StoreMigration : DataMigration<Settings> {
+
+    /**
+     * The current version of the datastore schema. Increase this when migration is necessary,
+     * e.g. when you add a new field, to ensure a correct default value is set instead of the
+     * Protobuf default value for that data type like "" or 0, 0.0, etc.
+     */
+    private val currentVersion = 1
+
+    override suspend fun shouldMigrate(currentData: Settings): Boolean {
+        // When no previous datastore file exists, the version starts at 0, i.e. this ensures
+        // that the correct default values are set instead of the Protobuf default value for that
+        // data type like "" or 0, 0.0, etc.
+        return currentData.version < currentVersion
+    }
+
+    override suspend fun migrate(currentData: Settings): Settings {
+        val currentVersion = currentData.version
+        val targetVersion = currentVersion + 1
+        val logTemplate =
+            "Migrating ${Settings::class.java.name} from $currentVersion to $targetVersion"
+        Log.i(TAG, String.format(logTemplate, currentVersion, targetVersion))
+
+        val builder = currentData.toBuilder()
+        when (currentVersion) {
+            0 -> {
+                @Suppress("UsePropertyAccessSyntax")
+                builder
+                    .setCenterMap(DEFAULT_CENTER_MAP)
+                    .setUploadEnabled(DEFAULT_SYNCHRONIZATION)
+                    .setSensorFrequency(DEFAULT_SENSOR_FREQUENCY)
+                    .setReportErrors(DEFAULT_REPORTING)
+                    .setModality(DEFAULT_MODALITY)
+                    .setAcceptedTerms(DEFAULT_ACCEPTED_TERMS)
+            }
+
+            else -> {
+                throw MigrationException("No migration code for version ${currentVersion}.")
+            }
+        }
+        return builder.build()
+    }
+
+    override suspend fun cleanUp() {
+        // Is called when migration was successful
+        // Currently, there is no cleanup to do, yet
+    }
+
+    companion object {
+        /**
+         * The boolean value indicating whether the map should be centered.
+         */
+        private const val DEFAULT_CENTER_MAP = true
+
+        /**
+         * The boolean value indicating whether data should be uploaded or synchronized.
+         */
+        private const val DEFAULT_SYNCHRONIZATION = true
+
+        /**
+         * `true` if errors should be reported.
+         */
+        private const val DEFAULT_REPORTING = false
+
+        /**
+         * The API version of the terms accepted by the user, e.g. 5.
+         */
+        private const val DEFAULT_ACCEPTED_TERMS = 0
+
+        /**
+         * The maximum frequency for each sensor type to collect data with.
+         */
+        private const val DEFAULT_SENSOR_FREQUENCY = 100
+
+        /**
+         * When no modality is selected yet or when the previous modality is now unsupported.
+         */
+        private const val DEFAULT_MODALITY = "UNKNOWN"
+    }
+}
